ca726f1b50a6a30c489f62d675d0b40d1f474c18
Akmal khan <akmalkhan.ak426@gmail.com>
2024-05-20 06:46:43 +0500

    login functionality implemented

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,25 +2,38 @@ import bcrypt from 'bcrypt'
 import prisma from '../lib/prisma.js'
 
 export const register = async( req, res) =>{
-   const {username, email, password, avatar} = req.body
+  try {
+    const {username, email, password, avatar} = req.body
    
-   const hashPassword = await bcrypt.hash(password, 10)
-   
-   const newUser = await prisma.user.create({
-    data:{
-        username,
-        email,
-        password:hashPassword,
-        avatar
-    }
-   })
+    const hashPassword = await bcrypt.hash(password, 10)
     
-   return res.status(200).json({newUser, message:"User created successfully"});
+    const newUser = await prisma.user.create({
+     data:{
+         username,
+         email,
+         password:hashPassword,
+         avatar
+     }
+    })
+     
+    return res.status(200).json({newUser, message:"User created successfully"});
+  } catch (error) {
+     res.status(501).json({message: error})
+  }
 }
 
 
-export const login = (req, res) =>{
-    console.log('login clicked')
+export const login = async(req, res) =>{
+   try {
+    const {username, password} = req.body
+    const existingUser = await prisma.user.findUnique({where:{username}})
+    if (!existingUser) return res.status(401).json({message: "Invalid Credentials"});
+    const isMatch = await bcrypt.compare(existingUser.password, password)
+    if(!isMatch) 
+        return res.status(401).json({message:"Invalid Credentials"});
+   } catch (error) {
+     res.status(501).json({message:"Internal error ", error})
+   }
 }
 
 
